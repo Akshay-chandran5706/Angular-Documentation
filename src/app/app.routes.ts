b8006029ae55c1c965b22419c246c41b3f75cdb2
Akshay-chandran5706 <akshaypty5706@gmail.com>
2025-07-07 16:48:41 +0530

    components renamed to menus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,17 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () =>
-            import('./components/landing-page/landing-page.component').then(m => m.LandingPageComponent)
+            import('./menus/landing-page/landing-page.component').then(m => m.LandingPageComponent)
     },
     {
         path: 'angular-basics',
         loadComponent: () =>
-            import('./components/angular-basics/angular-basics.component').then(m => m.AngularBasicsComponent),
+            import('./menus/angular-basics/angular-basics.component').then(m => m.AngularBasicsComponent),
         children: [
             {
                 path: 'cli',
                 loadComponent: () =>
-                    import('./components/angular-basics/cli-setup/cli-setup.component').then(m => m.CliSetupComponent)
+                    import('./menus/angular-basics/cli-setup/cli-setup.component').then(m => m.CliSetupComponent)
             }
         ]
     },
